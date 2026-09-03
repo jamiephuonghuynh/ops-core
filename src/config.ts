6342617160abb2853d45356d0d34000a42d010b9
jamiephuonghuynh <jamiephuonghuynh@gmail.com>
2026-09-03 19:57:25 +0700

    Add files via upload

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SERVICE_NAME = "ops-core-dev";
-export const BUILD_VERSION = "OPS-CF-CEF01";
-export const CODE_BASE = "030926_1615";
+export const BUILD_VERSION = "OPS-CF-CEF01-HF01";
+export const CODE_BASE = "030926_1936";
 export const SCHEMA_VERSION = "0001";
 export const ENVIRONMENT = "DEV";
 export const WORKFLOW_RETRY_LIMIT = 3;
